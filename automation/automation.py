593fc261c41d9e573005ab1708c121532526ef3e
samksenija <ksenijasamardzic4@gmail.com>
2026-06-01 10:19:22 +0200

    query_execution_time, further improvements into automation

--- automation/automation.py
+++ automation/automation.py
@@ -1,11 +1,19 @@
 import connection
-from utils import flush_cache
+
+from utils import flush_cache, query_execution_time
 
 cursor = connection.cursor
 
 flush_cache(cursor)
 
-query = "SELECT * FROM tpch.orders WHERE O_ORDERKEY = 290 AND O_ORDERSTATUS = 'F'"
+query = "SELECT * FROM orders where O_CUSTKEY = 1910 AND O_ORDERPRIORITY = '5-LOW';"
+
+identifier = 1
+layer_tag = '1st'
+change_applied = "Change of filter column order"
+note = None
+duration = query_execution_time(cursor, query)
+
 results = []
 
 queries = [
@@ -24,16 +32,18 @@ insert_query = """
     explain_tree, explain_analyze, duration, layer_tag, change_applied, note) 
     VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s)
     """
-values = (2, 
-    str(query), 
+values = (
+    identifier, 
+    query, 
     str(results[0][0]), 
-    str(results[1][0]), 
-    str(results[2][0]), 
-    str(results[3][0]), 
-    None, 
-    'original',
-    None, 
-    "Primary key (O_ORDERKEY) is always index as well, so optimizator will use it first when performing optimization, no matter where it's ordered in query.")
+    results[1][0][0], 
+    results[2][0][0], 
+    results[3][0][0], 
+    duration, 
+    layer_tag,
+    change_applied, 
+    note
+    )
 
 insert_into_statistics = cursor.execute(insert_query, values) 
 connection.connection.commit()
--- automation/utils.py
+++ automation/utils.py
@@ -1,3 +1,15 @@
+import time
+
+# Define the parameters for references
+changes_applied = [None, "Change of filter column order"]
+notes = [
+    None,
+    "How filter order when no primary key is used affects the execution.", 
+    "Primary key (O_ORDERKEY) is always index as well, so optimizator will use it first when performing optimization, no matter where it's ordered in query."
+]
+layer_tags = ["original", "1st", "2nd"]
+
+
 # In order to preform true benchmarking, we need to flush the cache before each test. 
 # This is a simple function that does just that.
 def flush_cache(cursor):
@@ -5,3 +17,16 @@ def flush_cache(cursor):
     cursor.execute("FLUSH STATUS;")
     
     print("Cache flushed successfully.")
+    
+
+# Even although this parameter can't be too relevant atm,
+# It would be nice to see db execution time as captured parameter if needed to be referenced further
+def query_execution_time(cursor, query):
+    start_time = time.perf_counter()
+    cursor.execute(query)
+    cursor.fetchall()
+    end_time = time.perf_counter()
+
+    execution_time = end_time - start_time
+    
+    return f"{execution_time:.6f} sec"

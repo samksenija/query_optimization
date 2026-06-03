482dc9f16e34e365856842e8a4d24f08b7efb638
samksenija <ksenijasamardzic4@gmail.com>
2026-06-03 20:40:10 +0200

    second edition

--- automation/patterns.py
+++ automation/patterns.py
@@ -1,8 +1,9 @@
+# Here column names are extracted in order for further processing to occur
 import re
 
 query = "SELECT * FROM orders where O_ORDERPRIORITY = '5-LOW' AND O_CUSTKEY = 1910 AND TEST = 15;"
 
-extract_column_names_first_step = re.findall('((AND )|(where )|(WHERE )|(and ))(\w+)( =)', query)
+extract_column_names_first_step = re.findall('([(AND )]|[(where )]|[(WHERE )]|[(and )]|[(\s)])(\w+)( =)', query)
 print(extract_column_names_first_step)
 
 

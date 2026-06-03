4cbc041a53d25b18fbe92a69e6966d22e38d0f3b
samksenija <ksenijasamardzic4@gmail.com>
2026-06-03 20:31:34 +0200

    include lower case 'and'

--- automation/patterns.py
+++ automation/patterns.py
@@ -2,7 +2,7 @@ import re
 
 query = "SELECT * FROM orders where O_ORDERPRIORITY = '5-LOW' AND O_CUSTKEY = 1910 AND TEST = 15;"
 
-extract_column_names_first_step = re.findall('((AND )|(where )|(WHERE ))(\w+)( =)', query)
+extract_column_names_first_step = re.findall('((AND )|(where )|(WHERE )|(and ))(\w+)( =)', query)
 print(extract_column_names_first_step)
 
 

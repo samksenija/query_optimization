a767efa3548304539bd96e821343f69504b17087
samksenija <ksenijasamardzic4@gmail.com>
2026-06-13 11:08:52 +0200

    extract_column_names RegEx

--- automation/patterns.py
+++ automation/patterns.py
@@ -4,18 +4,17 @@ import re
 query = "SELECT * FROM orders where O_ORDERPRIORITY = '5-LOW' AND O_CUSTKEY = 1910 AND TEST = 15;"
 
 extract_column_names_first_step = re.findall('([(AND )]|[(where )]|[(WHERE )]|[(and )]|[(\s)])(\w+)( =)', query)
-# print(extract_column_names_first_step)
 
 column_names = []
 
 # Here just column names are extracted without the other additional matthes
-def extract_just_column_name(extract_column_names_first_step):
+def extract_column_names(extract_column_names_first_step):
     for result in extract_column_names_first_step:
         column_names.append(result[1])
         
     print(column_names)
+  
+extract_column_names(extract_column_names_first_step)
 
-    
-extract_just_column_name(extract_column_names_first_step)
 
 

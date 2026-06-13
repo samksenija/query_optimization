50856a4260b0f934af04de64a9673d93fbc73f79
samksenija <ksenijasamardzic4@gmail.com>
2026-06-13 11:36:59 +0200

    RegEx case insensitive

--- automation/patterns.py
+++ automation/patterns.py
@@ -9,9 +9,9 @@ joined_table_names = []
 query = "SELECT * FROM orders where O_ORDERPRIORITY = '5-LOW' AND O_CUSTKEY = 1910 AND TEST = 15;"
 query_multitable_join = "SELECT c.name, o.order_id, p.product_name, oi.quantity, p.price FROM customers c JOIN orders o ON c.customer_id = o.customer_id JOIN order_items oi ON o.order_id = oi.order_id JOIN products p ON oi.product_id = p.product_id;"
 
-extract_column_names_first_step = re.findall('(where| AND| WHERE) (\w+)', query)
-extract_main_table_name_first_step =  re.findall('FROM (\w+)', query)
-extract_join_table_names_first_step = re.findall('JOIN (\w+)', query_multitable_join)
+extract_column_names_first_step = re.findall('(?i)(where| and) (\w+)', query)
+extract_main_table_name_first_step =  re.findall('(?i)FROM (\w+)', query)
+extract_join_table_names_first_step = re.findall('(?i)JOIN (\w+)', query_multitable_join)
 
 # Here just column names are extracted without the other additional matches
 def extract_column_names(extract_column_names_first_step):

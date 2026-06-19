69d0c43973db81bcb08ebece4eff6b0ee3000834
samksenija <ksenijasamardzic4@gmail.com>
2026-06-19 12:24:03 +0200

    queries key set up

--- automation/queries.py
+++ automation/queries.py
@@ -1,11 +1,11 @@
 queries = {
-    1: {
+    "1_0": {
         "query": "SELECT * FROM orders where O_ORDERPRIORITY = '5-LOW' AND O_CUSTKEY = 1910;",
         "layer_tag": "original",
         "change_applied": None,
         "note": None
     },
-    1: {
+    "1_1": {
         "query": "SELECT * FROM orders where O_CUSTKEY = 1910 AND O_ORDERPRIORITY = '5-LOW';",
         "layer_tag": "1st",
         "change_applied": "Change of filter column order",
